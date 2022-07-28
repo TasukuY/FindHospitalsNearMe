a0470834c9af590a2fa065204751647c0a461dff
TasukuY <alohaty81@gmail.com>
2022-07-28 00:52:00 -0600

    Initial Commit

---
+++ HospitalNearUsers/HospitalNearUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HospitalNearUsersApp.swift
+//  HospitalNearUsers
+//
+//  Created by Tasuku Yamamoto on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct HospitalNearUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
